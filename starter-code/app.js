eee5c5b93a053e601259931aaae2c82d7161e3d0
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-05-01 16:25:58 -0700

    solve problem 4

--- starter-code/app.js
+++ starter-code/app.js
@@ -75,15 +75,17 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-// var testArray = [2, 3, 4]; //eslint-disable-line
+var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  // arraySum = sum(sum(sumArr[0],sumArr[1]),sumArr[2]);
+  var arraySum = sum(sum(sumArr[0],sumArr[1])[0], sumArr[2])[0];
+  var arrayString = sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + arraySum + ' is their sum.';
+  return [arraySum,arrayString];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
