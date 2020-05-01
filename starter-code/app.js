b2696c535ceec11f3c2c646cfd26ebf4b7135883
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-05-01 16:01:40 -0700

    Solve problem 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -52,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var addInput = a + b + c;
+  var multiplyInput = a * b * c;
+  var addString = a + ' and ' + b + ' and ' + c + ' sum to ' + addInput + '.';
+  var multiplyString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyInput + '.';
+  return [addInput,multiplyInput,addString,multiplyString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -74,7 +78,7 @@ Test this function by hand in the console to get it working, and when you think 
 // var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  // arraySum = sum(sum(sumArr[0],sumArr[1]),sumArr[2]);
 }
 
 // Here is the test for sumArray(); uncomment it to run it
